4cdc91daa093b682927890277e4759c45b73b4f0
Yılmaz Ev <81462055+yilmazev@users.noreply.github.com>
2023-02-16 21:33:04 +0300

    Design rearranged

---
+++ src/pages/Home/Views/About/AboutView.js
@@ -0,0 +1,34 @@
+import * as Styled from "./AboutStyle"
+import AboutImage from "../../images/TXxiFuQLBKQ.jpg"
+import { ArrowRightLine } from "../../../../components/Icons/IconsView"
+import { NavLink } from "react-router-dom"
+
+export default function About() {
+    return(
+        <Styled.About id="about">
+            <Styled.Container>
+                <Styled.ImageContent>
+                    <Styled.Image src="https://ld-wp73.template-help.com/wordpress/prod_5372/v1/wp-content/uploads/2019/10/img-1.jpg" />
+                    <Styled.ImageAbsolute />
+                </Styled.ImageContent>
+                <Styled.TextWrapper>
+                    <Styled.TextContent>
+                        <Styled.Title>We are an it-consulting company.</Styled.Title>
+                        <Styled.Subtitle>Owning more than hundred years experience in all areas of IT.</Styled.Subtitle>
+                        <Styled.Paragraph>
+                            A company's most valuable asset is its people. We are proud to have a highly motivated and qualified team that supports our customers in reaching their goals. Our team members have the right mix of technological expertise and deep understanding of customer businesses. They work diligently every day to ensure our customers' projects are successful.
+                            <br /><br />
+                            We aim to be one of the leading IT service providers in the world and are working non stop on delivering the required service and value to our customers. We are committed in helping our customers achieve their business goals through innovative ideas, forward-thinking strategies, and perfectly tailored IT solutions. Our success is built on the passion of our people – we would be nothing without the hard work and dedication of our talented team members. We value team-work and was able to create a great atmosphere and team-spirit for which we continuously look to expand it with talented new work colleagues.
+                        </Styled.Paragraph>
+                        <NavLink className="button-group" to="/about">
+                            <Styled.ButtonIcon>
+                                <ArrowRightLine width="1.5rem" height="1.5rem" fill="none" stroke="#FFFFFF" strokeWidth="2px" />
+                            </Styled.ButtonIcon>
+                            <Styled.ButtonText>More details</Styled.ButtonText>
+                        </NavLink>
+                    </Styled.TextContent>
+                </Styled.TextWrapper>
+            </Styled.Container>
+        </Styled.About>
+    )
+}
